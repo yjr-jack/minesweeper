33c9032acf3de20ff5a822c327d9c1d07b4d3a92
yjr-jack <yjr-jack@qq.com>
2025-01-27 01:18:10 +0800

    更新

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is a minesweeper game.
+Some minesweeper games.
---
+++ c/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "a.h"
+#include <time.h>
+#include <stdlib.h>
+#define X 30
+#define Y 20
+#define B 10
+char c[X][Y];//空
+char e[X][Y];//0
+char g[X][Y];//计0
+char h[X][Y];//统计
+
+int main (){
+    fa();
+    fb();
+    fd();
+    fc(c);
+    fe();
+    return 0;
+}
